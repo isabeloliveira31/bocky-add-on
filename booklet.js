a41432ee7dcade77012cd5ede154893235ad1590
Isabel Oliveira <isabel.ribeiro.oliveira@superbockgroup.com>
2025-08-04 17:44:11 +0100

    converted // comments on booklet to /* */ and improved some paddings and fontsizes

--- booklet.js
+++ booklet.js
@@ -8,18 +8,18 @@ javascript:(function(){
         position: 'fixed', 
         border: 'none', 
         zIndex: '2147483647', 
-        // MEDIDAS ANTIGAS
+        /* MEDIDAS ANTIGAS */
         height: 'calc(7vh + 1.3rem)', 
         width: 'calc(7vh + 1.3rem)', 
         bottom: 'min(50px, 5%)', 
         right: 'min(40px, 4%)',
-        // MEDIDAS FIGMA
-        // height: 'calc(7.1vh + 1.35rem)', /* adapted from 88px */
-        // width: 'calc(7.1vh + 1.35rem)',
-        // bottom: 'calc(0.5vh + 0.2rem)',  /* adapted from 8px */
-        // right: 'calc(0.5vh + 0.2rem)',
+        /* MEDIDAS FIGMA */
+        /* height: 'calc(7.1vh + 1.35rem)', */ /* adapted from 88px */
+        /* width: 'calc(7.1vh + 1.35rem)', */
+        /* bottom: 'calc(0.5vh + 0.2rem)', */ /* adapted from 8px */
+        /* right: 'calc(0.5vh + 0.2rem)', */
         background: 'none', 
-        backgroundColor: 'darkBlue'
+        backgroundColor: 'none'
     }); 
     document.body.appendChild(iframe); 
     window.addEventListener('message', (event) => { 
@@ -40,9 +40,9 @@ javascript:(function(){
                 iframe.style.width = '25vw';
                 console.log('size E (> 1550)');
             }
-            //iframe.style.width = 'clamp(20vw, 70rem,90vw)'; // 10vw+12rem  // max(20vw,200rem)
-            //iframe.style.width = event.data.width + 'px'; 
-            iframe.style.height = event.data.height + 'px'; // use clamp with visualViewport.height * 0.9 as max
+            /* iframe.style.width = 'clamp(20vw, 70rem,90vw)'; OR 10vw+12rem  OR max(20vw,200rem) */
+            /* iframe.style.width = event.data.width + 'px'; */
+            iframe.style.height = event.data.height + 'px'; /* use clamp with visualViewport.height * 0.9 as max */
         } else if (event.data.type === 'collapse-bocky') { 
             iframe.style.width = 'calc(6.5vh + 1rem)';
             iframe.style.height = 'calc(6.5vh + 1rem)';
